85a9edbb8adc0598c2c6e69f4d85fe698fc6ebf1
gaohang <isaackao@foxmail.com>
2020-05-29 01:27:29 +0800

    Nav.tsx引入svg

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
 
+require('icons/label.svg');
+require('icons/statistic.svg');
+require('icons/money.svg');
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,.25);
@@ -11,8 +15,14 @@ const NavWrapper = styled.nav`
       user-select: none;
       text-align:center;
       width: 33.33%;
-      padding-top: 16px;
-      padding-bottom: 16px;
+      padding: 4px 0;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -22,13 +32,22 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Link to="/tags">标签页面</Link>
+                    <svg className="icon">
+                        <use xlinkHref={"#label"} />
+                    </svg>
+                    <Link to="/tags">标签</Link>
                 </li>
                 <li>
-                    <Link to="/money">记录页面</Link>
+                    <svg className="icon">
+                        <use xlinkHref={"#money"} />
+                    </svg>
+                    <Link to="/money">记录</Link>
                 </li>
                 <li>
-                    <Link to="/statistic">数据页面</Link>
+                    <svg className="icon">
+                        <use xlinkHref={"#statistic"} />
+                    </svg>
+                    <Link to="/statistic">数据</Link>
                 </li>
             </ul>
         </NavWrapper>
